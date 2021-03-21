192da64041cc335eca2caa1782930187b6e035c8
copetre <63053696+copetre@users.noreply.github.com>
2021-03-21 01:58:36 +0100

    Start

--- Save The Count/main.py
+++ Save The Count/main.py
@@ -14,15 +14,15 @@ clock = pygame.time.Clock()
 
 running = True  # Jeu en cours
 
-background = pygame.image.load('asset/hall.jpg')
-
-arrow = pygame.transform.scale(pygame.image.load('asset/arrow.png'),(200,100))
+background = pygame.image.load('asset/FIRST_SCREEN.png')
+firstsreen = [pygame.image.load('asset/FIRST_SCREEN.png'), pygame.image.load('asset/FIRST_SCREEN_TEXT.png')]
+frame = 0
 
 hud = HUD()
 
 # Création des personnages
 game = Game(hud)
-
+arrow = pygame.transform.scale(pygame.image.load('asset/arrow.png'), (200, 100))
 # toutes les 1 seconde, on augmente aléatoirement la barre
 randomVotebarIncrease = pygame.USEREVENT + 1
 pygame.time.set_timer(randomVotebarIncrease, 1000)
@@ -34,6 +34,11 @@ channelFond = pygame.mixer.Channel(1)
 # Sound at first
 channelFond.play(pygame.mixer.Sound('SoundMusic/NiveauBureauVotes2.mp3.ogg'), -1)
 
+surface = pygame.Surface((800, 90))
+surface.fill((128, 128, 128))
+rectangle = surface.get_rect(left=100, top=420)  # alternative = (center = (608, 32))
+frame = 0
+
 while running:
     clock.tick(60)
 
@@ -41,11 +46,11 @@ while running:
 
     screen.blit(background, (0, 0))
 
-    if game.level[0]:
-
-        background = pygame.transform.scale(pygame.image.load('asset/hall.jpg'), (1024, 576))
-
-        if game.player.rect.x > 900 and game.count_policiers == 0 :
+    if not game.playing:
+        frame = (frame + 1) % 12  # %12 because we have 2 frames * 6 ticks each
+        background = firstsreen[frame // 6]
+    else:
+        if game.level[0] and game.player.rect.x > 900 and game.count_policiers == 0:
             game.level[0] = False
             game.level[1] = True
             game.player.rect.x = 0
@@ -53,95 +58,82 @@ while running:
             game.count_policiers = 1
             game.spawn_senator_red()
             game.count_senator_red = 1
+            background = pygame.transform.scale(pygame.image.load('asset/exterior.jpg'), (1024, 576))
 
-
-    elif game.level[1]:
-
-        game.player.velocity = 8
-        if game.player.rect.x > 900:
+        elif game.level[1] and game.player.rect.x > 900:
             game.level[1] = False
             game.level[2] = True
             game.player.rect.x = 20
             game.spawn_policier()
             game.spawn_matraque()
             game.count_policiers = 2
-    elif game.level[2]:
-        background = pygame.transform.scale(pygame.image.load('asset/hall.jpg'), (1024, 576))
-        if game.player.rect.x > 900:
+            background = pygame.transform.scale(pygame.image.load('asset/hall.jpg'), (1024, 576))
+
+        elif game.level[2] and game.player.rect.x > 900 and game.count_policiers == 0:
             game.level[2] = False
             game.level[3] = True
             game.player.rect.x = 20
+            background = pygame.image.load('asset/chamber.jpg')
 
-    elif game.level[3]:
-        background = pygame.image.load('asset/hall.jpg')
-        if game.player.rect.x > 900:
-            game.level[3] = False
-            game.level[4] = True
-            game.player.rect.x = 0
+        if game.count_policiers == 0:
+            screen.blit(arrow, (800, 200))
 
-    elif game.level[4]:
-        background = pygame.image.load('asset/exterior.png')
-        game.player.rect.x = 0
-        
-    if game.count_policiers == 0:
-        screen.blit(arrow, (800, 200))
-    
-    # HUD
-    hud.refresh(screen)
+        # HUD
+        hud.refresh(screen)
 
-    # Policiers pan pan
-    for police in game.all_policiers:
-        # draw policier
-        police.refresh(screen)
-        police.all_bullets.draw(screen)
-        # do actions
-        if not(police.dead):
-            police.move()
-            police.randomFire()
-        for bullet in police.all_bullets :
-            bullet.move(game.player)
+        # Policiers pan pan
+        for police in game.all_policiers:
+            # draw policier
+            police.refresh(screen)
+            police.all_bullets.draw(screen)
+            # do actions
+            if not (police.dead):
+                police.move()
+                police.randomFire()
+            for bullet in police.all_bullets:
+                bullet.move(game.player)
 
-    # Policiers bim bam boum
-    for police in game.all_matraque:
-        police.refresh(screen)
-        # do actions
-        if not(police.dead):
-            police.move()
-            police.randomAttack()
+        # Policiers bim bam boum
+        for police in game.all_matraque:
+            police.refresh(screen)
+            # do actions
+            if not (police.dead):
+                police.move()
+                police.randomAttack()
 
-    # Senators
-    for senablue in game.all_senblue:
-        senablue.refresh(screen)
-        if not(senablue.dead):
-            senablue.move()
+        # Senators
+        for senablue in game.all_senblue:
+            senablue.refresh(screen)
+            if not (senablue.dead):
+                senablue.move()
 
-    for senared in game.all_senred:
-        senared.refresh(screen)
-        if not(senared.dead):
-            senared.move()
+        for senared in game.all_senred:
+            senared.refresh(screen)
+            if not (senared.dead):
+                senared.move()
 
-    # Jack
-    game.player.refresh(screen)
+        # Jack
+        game.player.refresh(screen)
 
-    # Mouvements de Jack
-    if not(game.player.dead):
-        # JUMP
-        if game.pressed.get(pygame.K_UP):
-            game.player.jump()
-        game.player.gravity()
-        # RIGHT
-        if game.pressed.get(pygame.K_RIGHT) and game.player.rect.x < 970 :
-            game.player.right()
-        else:
-            game.player.walkAnimationRight = False
-        # LEFT
-        if game.pressed.get(pygame.K_LEFT) and game.player.rect.x > -10:
-            game.player.left()
-        else:
-            game.player.walkAnimationLeft = False
-        # ATTACK
-        if game.pressed.get(pygame.K_SPACE):
-            game.player.launchAttack()
+        # Mouvements de Jack
+        if not (game.player.dead):
+            # JUMP
+            if game.pressed.get(pygame.K_UP):
+                game.player.jump()
+            game.player.gravity()
+            # RIGHT
+            if game.pressed.get(pygame.K_RIGHT) and game.player.rect.x < 970:
+                game.player.right()
+            else:
+                game.player.walkAnimationRight = False
+            # LEFT
+            if game.pressed.get(pygame.K_LEFT) and game.player.rect.x > -10:
+                game.player.left()
+            else:
+                game.player.walkAnimationLeft = False
+            # ATTACK
+            if game.pressed.get(pygame.K_SPACE):
+                game.player.launchAttack()
 
     pygame.display.flip()
 
@@ -150,14 +142,18 @@ while running:
             running = False
             pygame.quit()
 
-        elif event.type == pygame.KEYDOWN : #On appuie sur une touche
-            game.pressed[event.key] = True #On reste appuyé sur une touche
-        elif event.type == pygame.KEYUP :
-            game.pressed[event.key] = False #On lache la touche
+        elif event.type == pygame.KEYDOWN:  # On appuie sur une touche
+            game.pressed[event.key] = True  # On reste appuyé sur une touche
+        elif event.type == pygame.KEYUP:
+            game.pressed[event.key] = False  # On lache la touche
 
-        elif not(game.player.dead) and event.type == randomVotebarIncrease: # on augmente la barre aléatoirement
-            rand = random.random() # nombre entre 0 et 1
-            if(rand<0.5): # 50% d'augmenter les bleus
+        elif not (game.player.dead) and event.type == randomVotebarIncrease:  # on augmente la barre aléatoirement
+            rand = random.random()  # nombre entre 0 et 1
+            if (rand < 0.5):  # 50% d'augmenter les bleus
                 hud.votebar.blueInc()
             elif (rand < 0.7):  # 20% d'augmenter les rouges
                 hud.votebar.redInc()
+        elif event.type == pygame.MOUSEBUTTONDOWN and game.playing == False :
+            if rectangle.collidepoint(event.pos) :
+                game.playing = True
+                background = pygame.image.load('asset/background.png')
